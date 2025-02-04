b50b8eb7e2ef6531e2d7594e849ea8d90da862d0
Nando Schürmann <nando.schuermann@getabstract.com>
2025-02-04 13:38:17 +0100

    Add TaskAdder.vue

---
+++ src/playground/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import TaskAdder from "./components/TaskAdder.vue"
+  import {ref} from "vue";
+
+  const tasks = ref([
+      "Rasen mähen",
+      "Spanisch lernen",
+  ])
+</script>
+
+<template>
+  <TaskAdder @add="tasks.push($event)"></TaskAdder>
+  <dl>
+    <dd v-for="task in tasks" :key="task">
+      {{ task }}
+    </dd>
+  </dl>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/playground/components/TaskAdder.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import {ref} from "vue";
+
+  defineEmits(['add'])
+
+  const task = ref("")
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="task"/>
+    <button @click="$emit('add', task)">Add Task</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
